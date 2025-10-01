5047cbcb8d55b67a66878a75cefaa06e0eba5865
emmdaz <emmanuel.cruzd@alumno.buap.mx>
2025-10-01 14:50:56 -0600

    Se documentó el código del modelado de la función.

--- Function-Modeling.py
+++ Function-Modeling.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import Layer
@@ -12,9 +6,6 @@ from tensorflow.keras.optimizers import SGD, Adam
 import numpy as np
 import matplotlib.pyplot as plt 
 import math as mt
-
-
-# In[2]:
 
 
 loss_tracker = keras.metrics.Mean(name="loss")
@@ -35,54 +26,52 @@ class Function(keras.Model):
 
         return {"loss": loss_tracker.result()}
 
-
-# In[3]:
-
-
 class SinTransform(tf.keras.layers.Layer):
     def __init__(self, num_outputs):
         super(SinTransform,self).__init__()
         self.num_outputs = num_outputs
 
-        self.freq = tf.range(1., self.num_outputs + 1)
-
+        self.freq = tf.range(1., self.num_outputs + 1) 
+        # Aquí se definen las frecuencias para la función seno
+        # Se crea un tensor que va desde 1 hasta el último valor numérico 
+        # necesario para acompletar el 
+        # número de salidas que se tiene en la función. 
         self.kernel = self.add_weight("kernel",
                                 shape=[self.num_outputs])
+        # En esta parte se crearon los valores para los coefficientes que serán
+        # entrenables y calculables para la función que se desea modelar. 
+        # En este caso, corresponde cada uno de estos valores a los 
+        # valores de los pesos. 
 
     def call(self, inputs):
+        # Esta primer parte es para evitar problemas con la forma de 
+        # las entradas y corregir si es necesario
         inputs = tf.convert_to_tensor(inputs)
         if (inputs.shape == ()):
             inputs=(inputs,)
         elif (len(inputs.shape)==1):
             inputs=tf.expand_dims(inputs, axis=1)
+            
+        # La función calcula una serie (sum_i=1^N a_i*sin(w_i*x))
         batch = tf.shape(inputs)[0]
-        self.freq_b = tf.ones([batch,1])*self.freq
-        args = self.freq_b * inputs
-        modes = tf.math.sin(args)
-        return tf.tensordot(modes,self.kernel,1)
-
-
-# In[4]:
-
+        self.freq_b = tf.ones([batch,1])*self.freq 
+        # Crea un tensor cuyos elementos son 1's y multiplica cada uno
+        # por los valores de las frecuencias.
+        args = self.freq_b * inputs 
+        # Aquí se valcula el valor de las frecuencias (w_i*x)
+        modes = tf.math.sin(args) # Se calcula los valores de sin(w_i*x)
+        return tf.tensordot(modes,self.kernel,1)  # Se crea la serie de senos
 
 inputs = keras.Input(shape=(1,))
-print(inputs)
 x = SinTransform(5)(inputs)
+
 model = Function(inputs=inputs,outputs=x)
 model.summary()
-
-
-# In[11]:
-
 
 model.compile(optimizer=Adam(learning_rate=0.001), metrics=['loss'])
 
 x=tf.linspace(-1,1,100)
 history = model.fit(x,epochs=25,verbose=1)
-
-
-# In[13]:
-
 
 x_testv = tf.linspace(-1,1,100)
 a=model.predict(x_testv)
@@ -91,9 +80,6 @@ plt.plot(x_testv,a, label = "Predicción")
 plt.plot(x_testv, 3*tf.math.sin(mt.pi*x), label = "Función", color = "orange")
 plt.legend()
 plt.show()
-
-
-# In[ ]:
 
 
 
