13c4ad761c7914a75f8aaed234340a3306e00971
emmdaz <emmanuel.cruzd@alumno.buap.mx>
2025-10-01 00:47:21 -0600

    Segundo intento de modelar la función 1 + 2x² + 4x³. Se modificó el tamaño del batch que al parecer era el causante de provocar una especie de sobreajuste en el entrenamiento de la red que modela la función ya antes mencionada.

--- Function-Modeling-2.py
+++ Function-Modeling-2.py
@@ -1,17 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
 
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import Layer
 from tensorflow.keras.optimizers import SGD, Adam
+
 import numpy as np
 import matplotlib.pyplot as plt 
 import math as mt
+
+
+# In[2]:
+
 
 loss_tracker = keras.metrics.Mean(name="loss")
 
 class Function(keras.Model):
     def train_step(self, data):
-        batch_size = 5
+        batch_size = 10
         x = tf.random.uniform((batch_size,), minval=-1, maxval=1)
         eq = 1 + 2*x + 4*x**3
 
@@ -24,6 +34,10 @@ class Function(keras.Model):
         loss_tracker.update_state(loss)
 
         return {"loss": loss_tracker.result()}
+
+
+# In[3]:
+
 
 class SinTransform(tf.keras.layers.Layer):
     def __init__(self, num_outputs):
@@ -47,6 +61,10 @@ class SinTransform(tf.keras.layers.Layer):
         modes = tf.math.sin(args)
         return tf.tensordot(modes,self.kernel,1)
 
+
+# In[4]:
+
+
 inputs = keras.Input(shape=(1,))
 x = SinTransform(10)(inputs)
 model = Function(inputs=inputs,outputs=x)
@@ -55,6 +73,10 @@ model.compile(optimizer=SGD(learning_rate=0.1), metrics=['loss'])
 
 x=tf.linspace(-1,1,100)
 history = model.fit(x,epochs=50,verbose=1)
+
+
+# In[5]:
+
 
 x_testv = tf.linspace(-1,1,100)
 a=model.predict(x_testv)
